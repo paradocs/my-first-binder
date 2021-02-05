389b59437e4cf6a33423abb35947e23d07fdc97f
David Tan Creti <paradocs@frontiernet.net>
2021-02-05 13:11:55 -0600

    Add files via upload

---
+++ sqrt-object.jl
@@ -0,0 +1,70 @@
+### A Pluto.jl notebook ###
+# v0.12.20
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ eb3b3be0-67d1-11eb-144b-2f9581950503
+md"""
+#### Math can be easy!
+#### Just write simple steps.
+"""
+
+# ╔═╡ 66294030-67d3-11eb-25b1-8d25703546db
+md"""
+#### sqrt
+##### ??? What is this?
+####  Pluto 🛡️ to the rescue!
+#####  Doing Things can be objects.
+"""
+
+# ╔═╡ f4835c0a-67db-11eb-134c-85092b5a1ced
+md"""
+##### Looking for a short
+##### cut across a square 🔲?
+##### We call it the root 🥕.
+"""
+
+# ╔═╡ 1976e31c-67df-11eb-2d4a-1d8e612a99a9
+
+md"""
+##### sqrt() is a Doing Thing
+##### called a function.
+##### It can tells how far it
+##### is across a 1 by 1 🔲.
+"""
+
+
+# ╔═╡ 84554b8a-67ce-11eb-2e10-7550cbde4485
+md"""
+##### Hard to think of sqrt?
+##### I bet you will never forget
+## 🔲🥕
+##### Lets create a new Doing Thing.
+"""
+
+# ╔═╡ f8c8c828-67d0-11eb-16b6-f384a4a4a5df
+🔲🥕 = sqrt
+
+# ╔═╡ 279bdd4e-67e2-11eb-1adb-b98afc7e812e
+md"""
+
+##### Yes, it works!
+##### Will you remember?
+### 🔲🥕  $~~~~or~~~~~$   sqrt
+
+"""
+
+# ╔═╡ 1c3a7e52-67d1-11eb-0c5a-a5b36b4fc989
+🔲🥕(2)
+
+
+# ╔═╡ Cell order:
+# ╠═eb3b3be0-67d1-11eb-144b-2f9581950503
+# ╠═66294030-67d3-11eb-25b1-8d25703546db
+# ╠═f4835c0a-67db-11eb-134c-85092b5a1ced
+# ╠═1976e31c-67df-11eb-2d4a-1d8e612a99a9
+# ╠═84554b8a-67ce-11eb-2e10-7550cbde4485
+# ╠═f8c8c828-67d0-11eb-16b6-f384a4a4a5df
+# ╠═279bdd4e-67e2-11eb-1adb-b98afc7e812e
+# ╠═1c3a7e52-67d1-11eb-0c5a-a5b36b4fc989
